d4474ad2112924202465bcb1d5dd4568031ed1de
dbrunson21 <brunsondavonte@gmail.com>
2023-10-03 22:14:47 -0400

    10/3 cram

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,17 @@ function getWeather(city) {
         })
         .then(function (data) {
             console.log(data)
+            //data for current weather
             getForcast(data.coord.lat, data.coord.lon)
-            document.querySelector("#cityname").textContent = data.name
+            document.querySelector("#cityname").textContent = data.name + " (" + new Date(data.dt * 1000).toLocaleDateString() + ")"
+            document.querySelector("#temp").textContent = "Temperature: " + data.main.temp + " F";
+            document.querySelector("#wind").textContent = "Wind: " + data.wind.speed + " MPH";
+            document.querySelector("#humidity").textContent = "Humidity: " + data.main.humidity + " %";
         })
+
 }
-getWeather("Denver")
+
+//display city on page
 
 //function for latitude and longitude for 5 day forcasts
 function getForcast(lat, lon) {
@@ -23,14 +29,41 @@ function getForcast(lat, lon) {
             return response.json()
         })
         .then(function (data) {
-            console.log(data.main)
+            console.log(data)
+
+            //data for 5 day forcast
+            for (let i = 0; i < data.list.length; i++) {
+                const element = data.list[i];
+                if (element.dt_txt.includes("12:00:00")) {
+                    console.log(element)
+                    var temp = document.createElement("p")
+                    temp.textContent = element.main.temp
+                    document.querySelector(".forcast").append(temp)
+                    var speed = document.createElement("p")
+                    speed.textContent = element.main.speed
+                    document.querySelector(".forcast").append(humidity)
+                    var humidity = document.createElement("p")
+                    humidity.textContent = element.main.humidity
+                    document.querySelector(".forcast").append(humidity)
+                    
+                }
+
+            }
         })
 }
 
 //Event Listener for button
 var searchBtn = document.getElementById("searchBtn");
-searchBtn.addEventListener("click", fetchData);
+searchBtn.addEventListener("click", function() {
+    fetchData();
+    
+    //Make border appear around content after search is clicked
+    var currentWeather = document.querySelector('.currentWeather')
+    currentWeather.classList.add('border');
+});
 
 function fetchData() {
-    var searchInput = document.getElementById("searchInput") 
+    var searchInput = document.getElementById("search")
+    console.log(searchInput.value)
+    getWeather(searchInput.value)
 }
